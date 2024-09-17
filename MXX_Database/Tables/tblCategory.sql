8d143dd28971cee5fb72fecac86d8ef24d6e71b9
Stefan Atanasovski <stef.atanasovski@hotmail.com>
2024-09-17 21:29:32 +0200

    Setup Database

---
+++ MXX_Database/Tables/tblCategory.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblCategory]
+(
+	[Id]                       INT IDENTITY (1, 1)                            NOT NULL,
+    [CreatedOn]                SMALLDATETIME                                  NOT NULL,
+    [DeletedOn]                SMALLDATETIME                                  NULL,
+    [IconFk]                   INT                                            NOT NULL,
+    [Name]                     VARCHAR(50)                                    NOT NULL,
+    [Type]                     INT                                            NOT NULL,
+    [SysStartTime]             DATETIME2 GENERATED ALWAYS AS ROW START        NOT NULL,
+    [SysEndTime]               DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN   NOT NULL,
+    PERIOD FOR SYSTEM_TIME ([SysStartTime],[SysEndTime]),
+    CONSTRAINT [PK_tblCategory] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_tblCategory_tblIcon] FOREIGN KEY ([IconFk]) REFERENCES [dbo].[tblIcon] ([Id]),
+)
+WITH
+(
+    SYSTEM_VERSIONING = ON (HISTORY_TABLE = [dbo].[tblCategoryHistory], DATA_CONSISTENCY_CHECK = ON)
+)
---
+++ MXX_Database/Tables/tblTransaction.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[tblTransaction]
+(
+    [Id]                       INT IDENTITY (1, 1)                            NOT NULL,
+    [CreatedOn]                SMALLDATETIME                                  NOT NULL,
+    [DeletedOn]                SMALLDATETIME                                  NULL,
+    [CategoryFk]               INT                                            NOT NULL,
+    [Name]                     VARCHAR(50)                                    NOT NULL,
+    [Type]                     INT                                            NOT NULL,
+    [Note]                     VARCHAR(50)                                    NOT NULL,
+    [Amount]                   MONEY                                          NOT NULL,
+    [SysStartTime]             DATETIME2 GENERATED ALWAYS AS ROW START        NOT NULL,
+    [SysEndTime]               DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN   NOT NULL,
+    PERIOD FOR SYSTEM_TIME ([SysStartTime],[SysEndTime]),
+    CONSTRAINT [PK_tblTransaction] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_tblTransaction_tblCategory] FOREIGN KEY ([CategoryFk]) REFERENCES [dbo].[tblCategory] ([Id]),
+
+)
+WITH
+(
+    SYSTEM_VERSIONING = ON (HISTORY_TABLE = [dbo].[tblTransactionHistory], DATA_CONSISTENCY_CHECK = ON)
+)
