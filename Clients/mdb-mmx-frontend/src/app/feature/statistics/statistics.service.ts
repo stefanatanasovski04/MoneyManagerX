e4f48a504db7f22a2697d1f178b6a545558f92b9
Stefan Atanasovski <stef.atanasovski@hotmail.com>
2024-11-02 04:00:52 +0100

    Add graphs

--- Clients/mdb-mmx-frontend/src/app/feature/statistics/statistics.service.ts
+++ Clients/mdb-mmx-frontend/src/app/feature/statistics/statistics.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, throwError } from 'rxjs';
+import { IEnvelope } from 'src/app/shared/models/dtos';
+import { IMonthlyIncomeExpenseDto, ITotalByCategoryResponse } from 'src/app/shared/models/responses';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatisticsService {
+    private baseUrl = 'http://localhost:5000/api';
 
-  constructor() { }
+      constructor(private http: HttpClient) {}
+
+      getTotalIncomePerCateogry(isYearly: boolean, currentMonth = new Date().toISOString().split('T')[0]) {
+          return this.http.get<IEnvelope<ITotalByCategoryResponse[]>>(`${this.baseUrl}/categories/total-per-cateogry?month=${currentMonth}&yearly=${isYearly}`)
+          .pipe(
+              map(response => response.result),
+              catchError(error => throwError(error))
+          );
+      }
+
+      getYearlyIncomesAndExpensesPerMonth(year: number = new Date().getFullYear()) {
+          return this.http.get<IEnvelope<IMonthlyIncomeExpenseDto[]>>(`${this.baseUrl}/statistics/yearly-income-expense?year=${year}`)
+          .pipe(
+              map(response => response.result),
+              catchError(error => throwError(error))
+          );
+      }
 }
